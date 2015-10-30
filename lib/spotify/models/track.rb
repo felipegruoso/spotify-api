64cdfacb41eacd8ac63e4a914ebb8d7ebad7e34c
Felipe Gadelha Ruoso <felipe.gruoso@gmail.com>
2015-10-30 18:45:45 +0000

    Fixes Track model.

--- lib/spotify/models/track.rb
+++ lib/spotify/models/track.rb
@@ -1,35 +1,39 @@
-module Models
+module Spotify
 
-  class Track
+  module Models
 
-    attr_reader :album, :artists, :external_ids, :external_urls,
-      :available_markets, :disc_number, :duration_ms, :explicit,
-      :href, :id, :is_playable, :linked_from, :name, :popularity,
-      :preview_url, :track_number, :type, :uri
+    class Track
 
-    def initialize(args = {})
-      args = args.with_indifferent_access
+      attr_reader :album, :artists, :external_ids, :external_urls,
+        :available_markets, :disc_number, :duration_ms, :explicit,
+        :href, :id, :is_playable, :linked_from, :name, :popularity,
+        :preview_url, :track_number, :type, :uri
 
-      @available_markets = args[:available_markets]
-      @disc_number       = args[:disc_number]
-      @duration_ms       = args[:duration_ms]
-      @explicit          = args[:explicit]
-      @href              = args[:href]
-      @id                = args[:id]
-      @is_playable       = args[:is_playable]
-      @linked_from       = args[:linked_from]
-      @name              = args[:name]
-      @popularity        = args[:popularity]
-      @preview_url       = args[:preview_url]
-      @track_number      = args[:track_number]
-      @type              = args[:type]
-      @uri               = args[:uri]
+      def initialize(args = {})
+        args = args.with_indifferent_access
 
-      # HACK: Using JSON while the other classes aren't available.
-      @album             = args[:album]
-      @artists           = args[:artists]
-      @external_ids      = args[:external_ids]
-      @external_urls     = args[:external_urls]
+        @available_markets = args[:available_markets]
+        @disc_number       = args[:disc_number]
+        @duration_ms       = args[:duration_ms]
+        @explicit          = args[:explicit]
+        @href              = args[:href]
+        @id                = args[:id]
+        @is_playable       = args[:is_playable]
+        @linked_from       = args[:linked_from]
+        @name              = args[:name]
+        @popularity        = args[:popularity]
+        @preview_url       = args[:preview_url]
+        @track_number      = args[:track_number]
+        @type              = args[:type]
+        @uri               = args[:uri]
+
+        # HACK: Using JSON while the other classes aren't available.
+        @album             = args[:album]
+        @artists           = args[:artists]
+        @external_ids      = args[:external_ids]
+        @external_urls     = args[:external_urls]
+      end
+
     end
 
   end

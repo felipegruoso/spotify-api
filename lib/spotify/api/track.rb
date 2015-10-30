d968bd9c0e8e9e8eb444c4bace995d16bef3df6f
Felipe Gadelha Ruoso <felipe.gruoso@gmail.com>
2015-10-30 20:00:32 +0000

    Adds some specs to Track requests.

--- lib/spotify/api/track.rb
+++ lib/spotify/api/track.rb
@@ -11,20 +11,23 @@ module Spotify
       #
       # Gets a track.
       #
-      # @param [Hash] args the search arguments
+      # @param [Hash] args the search arguments.
       # @option [Fixnum] :timeout the max time a request can take.
       # @option [Fixnum] :retries the number of retries if necessary.
       #
       # @return [Spotify::Models::Track] the extracted track.
       #
       def self.search_by_id(args = {})
-        self.new.search_by_id(args[:id])
+        service_params = args.slice(:timeout, :retries)
+        args           = args.slice(:id, :market)
+
+        self.new(service_params).search_by_id(args)
       end
 
       #
       # Gets several tracks.
       #
-      # @param [Hash] args the search arguments
+      # @param [Hash] args the search arguments.
       # @option [Fixnum] :timeout the max time a request can take.
       # @option [Fixnum] :retries the number of retries if necessary.
       #
@@ -32,22 +35,29 @@ module Spotify
       #   the extracted tracks.
       #
       def self.search_by_ids(args = {})
-        self.new.search_by_ids(args[:ids].join(','))
+        args[:ids]     ||= []
+        args[:ids]     = args[:ids].join(',')
+        service_params = args.slice(:timeout, :retries)
+        args           = args.slice(:ids, :market)
+
+        self.new(service_params).search_by_ids(args)
       end
 
       #
       # Gets a track.
       #
-      # @param [Hash] id the track id.
+      # @param [Hash] args the search arguments.
+      # @option [String] :id the track id.
+      # @option [String] :market the market.
       #
       # @return [Spotify::Models::Track] the extracted track.
       #
-      def search_by_id(id)
-        get(TRACKS_URL + '/' + id)
+      def search_by_id(args = {})
+        get(TRACKS_URL + '/' + args[:id].to_s, args.slice(:market))
 
         response = body
 
-        unless response[:error]
+        unless response["error"]
           response = Spotify::Models::Track.new(response)
         end
 
@@ -57,17 +67,19 @@ module Spotify
       #
       # Gets several tracks.
       #
-      # @param [Hash] ids the tracks ids between ",".
+      # @param [Hash] args the search arguments.
+      # @option [String] :ids the tracks ids between ",".
+      # @option [String] :market the market.
       #
       # @return [Array<Spotify::Models::Track>] an array containing
       #   the extracted tracks.
       #
-      def search_by_ids(ids)
-        get(TRACKS_URL, { ids: ids })
+      def search_by_ids(args)
+        get(TRACKS_URL, args)
 
         response = body
 
-        unless response[:error]
+        unless response["error"]
           response = response["tracks"].map do |track|
             Spotify::Models::Track.new(track)
           end
---
+++ spec/lib/api/track_spec.rb
@@ -0,0 +1,63 @@
+require 'spec_helper'
+
+describe Spotify::API::Track do
+
+  let(:id)     { '0eGsygTp906u18L0Oimnem' }
+  let(:ids)    { ["0eGsygTp906u18L0Oimnem", "1lDWb6b6ieDQ2xT7ewTC3G"] }
+  let(:market) { 'US' }
+
+  context "#search_by_id" do
+
+    example "Performs a simple request" do
+      track = described_class.search_by_id(id: id)
+
+      expect(track).to be_an_instance_of(Spotify::Models::Track)
+    end
+
+    example "Uses market parameter" do
+      track = described_class.search_by_id(id: id, market: market)
+
+      expect(track).to be_an_instance_of(Spotify::Models::Track)
+    end
+
+    example "Missing mandatory parameter (:id)" do
+      track = described_class.search_by_id(market: market)
+
+      expect(track).to be_an_instance_of(Hash)
+      expect(track['error']).to be_present
+    end
+
+  end
+
+  context "#search_by_ids" do
+
+    example "Performs a simple request" do
+      tracks = described_class.search_by_ids(ids: ids)
+
+      expect(tracks).to be_an_instance_of(Array)
+
+      tracks.each do |track|
+        expect(track).to be_an_instance_of(Spotify::Models::Track)
+      end
+    end
+
+    example "Uses market parameter" do
+      tracks = described_class.search_by_ids(ids: ids, market: market)
+
+      expect(tracks).to be_an_instance_of(Array)
+
+      tracks.each do |track|
+        expect(track).to be_an_instance_of(Spotify::Models::Track)
+      end
+    end
+
+    example "Missing mandatory parameter (:ids)" do
+      tracks = described_class.search_by_ids(market: market)
+
+      expect(tracks).to be_an_instance_of(Hash)
+      expect(tracks['error']).to be_present
+    end
+
+  end
+
+end

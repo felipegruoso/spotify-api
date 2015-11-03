7fa00181049642bbe13abf76c864616d4fb18916
Felipe <felipe.gruoso@gmail.com>
2015-11-03 11:16:08 +0000

    Uncomment artists specs.

--- spec/lib/api/artist_spec.rb
+++ spec/lib/api/artist_spec.rb
@@ -2,103 +2,103 @@ require 'spec_helper'
 
 describe Spotify::API::Artist do
 
-  let(:id)     { '0OdUWJ0sBjDrqHygGUXeCF' }
-  let(:ids)    { ['0oSGxfWSnnOXhD2fKuz2Gy', '3dBVyJ7JuOMt4GE9607Qin']}
+  let(:id)      { '0OdUWJ0sBjDrqHygGUXeCF' }
+  let(:ids)     { ['0oSGxfWSnnOXhD2fKuz2Gy', '3dBVyJ7JuOMt4GE9607Qin'] }
   let(:country) { 'US' }
 
-  # context '#search_by_id' do
+  context '#search_by_id' do
 
-  #   example "Performs a simple request" do
-  #     artist = described_class.search_by_id(id: id)
+    example "Performs a simple request" do
+      artist = described_class.search_by_id(id: id)
 
-  #     expect(artist).to be_an_instance_of(Spotify::Models::Artist)
-  #   end
+      expect(artist).to be_an_instance_of(Spotify::Models::Artist)
+    end
 
-  #   example "Missing mandatory parameter (:id)" do
-  #     artist = described_class.search_by_id
+    example "Missing mandatory parameter (:id)" do
+      artist = described_class.search_by_id
 
-  #     expect(artist).to be_an_instance_of(Hash)
-  #     expect(artist['error']).to be_present
-  #   end
+      expect(artist).to be_an_instance_of(Hash)
+      expect(artist['error']).to be_present
+    end
 
-  # end
+  end
 
-  # context "#search_by_ids" do
+  context "#search_by_ids" do
 
-  #   example "Performs a simple request" do
-  #     artists = described_class.search_by_ids(ids: ids)
+    example "Performs a simple request" do
+      artists = described_class.search_by_ids(ids: ids)
 
-  #     expect(artists).to be_an_instance_of(Array)
+      expect(artists).to be_an_instance_of(Array)
 
-  #     artists.each do |artist|
-  #       expect(artist).to be_an_instance_of(Spotify::Models::Artist)
-  #     end
-  #   end
+      artists.each do |artist|
+        expect(artist).to be_an_instance_of(Spotify::Models::Artist)
+      end
+    end
 
-  #   example "Missing mandatory parameter (:ids)" do
-  #     artist = described_class.search_by_ids
+    example "Missing mandatory parameter (:ids)" do
+      artist = described_class.search_by_ids
 
-  #     expect(artist).to be_an_instance_of(Hash)
-  #     expect(artist['error']).to be_present
-  #   end
+      expect(artist).to be_an_instance_of(Hash)
+      expect(artist['error']).to be_present
+    end
 
-  # end
+  end
 
-  # context "#top_tracks" do
+  context "#top_tracks" do
 
-  #   example "Performs a simple request" do
-  #     top_tracks = described_class.top_tracks(id: id, country: country)
+    example "Performs a simple request" do
+      top_tracks = described_class.top_tracks(id: id, country: country)
 
-  #     expect(top_tracks).to be_an_instance_of(Array)
+      expect(top_tracks).to be_an_instance_of(Array)
 
-  #     top_tracks.each do |top_track|
-  #       expect(top_track).to be_an_instance_of(Spotify::Models::Track)
-  #     end
-  #   end
+      top_tracks.each do |top_track|
+        expect(top_track).to be_an_instance_of(Spotify::Models::Track)
+      end
+    end
 
-  #   example "Missing mandatory parameter (:id)" do
-  #     top_tracks = described_class.top_tracks(country: country)
+    example "Missing mandatory parameter (:id)" do
+      top_tracks = described_class.top_tracks(country: country)
 
-  #     expect(top_tracks).to be_an_instance_of(Hash)
-  #     expect(top_tracks['error']).to be_present
-  #   end
+      expect(top_tracks).to be_an_instance_of(Hash)
+      expect(top_tracks['error']).to be_present
+    end
 
-  #   example "Missing mandatory parameter (:country)" do
-  #     top_tracks = described_class.top_tracks(id: id)
+    example "Missing mandatory parameter (:country)" do
+      top_tracks = described_class.top_tracks(id: id)
 
-  #     expect(top_tracks).to be_an_instance_of(Hash)
-  #     expect(top_tracks['error']).to be_present
-  #   end
+      expect(top_tracks).to be_an_instance_of(Hash)
+      expect(top_tracks['error']).to be_present
+    end
 
-  #   example "Missing all parameters (:id, :country)" do
-  #     top_tracks = described_class.top_tracks
+    example "Missing all parameters (:id, :country)" do
+      top_tracks = described_class.top_tracks
 
-  #     expect(top_tracks).to be_an_instance_of(Hash)
-  #     expect(top_tracks['error']).to be_present
-  #   end
+      expect(top_tracks).to be_an_instance_of(Hash)
+      expect(top_tracks['error']).to be_present
+    end
 
-  # end
+  end
 
-  # context "#related_artists" do
+  context "#related_artists" do
 
-  #   example "Performs a simple request" do
-  #     artists = described_class.related_artists(id: id)
+    example "Performs a simple request" do
+      artists = described_class.related_artists(id: id)
 
-  #     expect(artists).to be_an_instance_of(Array)
+      expect(artists).to be_an_instance_of(Array)
 
-  #     artists.each do |artist|
-  #       expect(artist).to be_an_instance_of(Spotify::Models::Artist)
-  #     end
-  #   end
+      artists.each do |artist|
+        expect(artist).to be_an_instance_of(Spotify::Models::Artist)
+      end
+    end
 
-  #   example "Missing mandatory parameter (:id)" do
-  #     artists = described_class.related_artists
+    example "Missing mandatory parameter (:id)" do
+      artists = described_class.related_artists
 
-  #     expect(artists).to be_an_instance_of(Hash)
-  #     expect(artists['error']).to be_present
-  #   end
+      expect(artists).to be_an_instance_of(Hash)
+      expect(artists['error']).to be_present
+    end
 
-  # end
+  end
 
   context "#albums" do
 
